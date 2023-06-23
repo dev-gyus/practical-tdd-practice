c7c74249564c80dc46f5a46420851dcc44616209
GyuHyeong <cjsworbehd13@gmail.com>
2023-06-23 21:04:17 +0900

    1. 테스트 추가

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockRepositoryTest.java
@@ -29,7 +29,7 @@ class StockRepositoryTest {
         // given
         Stock stock1 = Stock.create("001", 1);
         Stock stock2 = Stock.create("002", 2);
-        Stock stock3 = Stock.create("003", 3);
+        Stock stock3 = Stock.create("003", 3);;
         stockRepository.saveAll(List.of(stock1, stock2, stock3));
 
         // when
