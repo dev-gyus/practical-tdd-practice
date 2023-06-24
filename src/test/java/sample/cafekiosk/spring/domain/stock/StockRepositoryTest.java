48af101d8ca9e471ab0833dd45e3e1d5f9bae474
GyuHyeong <cjsworbehd13@gmail.com>
2023-06-24 18:44:17 +0900

    1. 테스트 수정

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockRepositoryTest.java
@@ -29,11 +29,11 @@ class StockRepositoryTest {
         // given
         Stock stock1 = Stock.create("001", 1);
         Stock stock2 = Stock.create("002", 2);
-        Stock stock3 = Stock.create("003", 3);;
+        Stock stock3 = Stock.create("003", 3);
         stockRepository.saveAll(List.of(stock1, stock2, stock3));
 
         // when
-        List<Stock> stocks = stockRepository.findAllByProductNumberIn(List.of("001", "002"));;
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(List.of("001", "002"));
 
         // then
         assertThat(stocks).hasSize(2)
