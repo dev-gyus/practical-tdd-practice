d1146600fda405cd44463d5a31122aa3e708ed83
GyuHyeong <cjsworbehd13@gmail.com>
2023-07-01 17:31:39 +0900

    1. 테스트수정

--- src/test/java/sample/cafekiosk/spring/api/controller/order/OrderControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/order/OrderControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 class OrderControllerTest {
     @Autowired
-    MockMvc mockMvc;
+    MockMvc mockMvc;;
     @Autowired
     OrderController orderController;
     @MockBean
